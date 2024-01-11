aa6c548d856d61146c98bc852ff619311bf58b98
Loh10 <93720015+loh10@users.noreply.github.com>
2024-01-11 15:30:43 +0100

    import model 3D

--- Alcanta-Pharmatech/Assets/DevRoomMap/Scripts/Casino.cs
+++ Alcanta-Pharmatech/Assets/DevRoomMap/Scripts/Casino.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class Casino : MonoBehaviour
 {
-    [SerializeField] GameObject casino;
+
+    [SerializeField]private GameObject casino;
+    [SerializeField]private List<string> possible = new List<string> {/*0*/ "rien", "rien", "rien", "rien", "rien", "rien",/*6*/"petit", "petit",/*8*/"grand",/*9*/ "moyen",/*10*/ "vie" };
+    [SerializeField]private string result1,result2,result3;
     public void OpenCasino()
     {
         casino.SetActive(true);
@@ -14,5 +17,61 @@ public class Casino : MonoBehaviour
     {
         casino.SetActive(false);
         Time.timeScale = 1;
+    }
+    public void Play()
+    {
+        result1 = ChooseLot();
+        result2 = ChooseLot();
+        result3 = ChooseLot();
+        if(result1 == result2&&result3 == result1)
+        {
+            Win(result1);
+        }
+        else
+        {
+            //loose
+        }
+    }
+    void Win(string lot)
+    {
+        switch (lot)
+        {
+            case "petit":
+                break;
+            case "moyen":
+                break;
+            case "grand":
+                break;
+            case "vie":
+                break;
+            default:
+                break;
+        }
+    }
+    private string ChooseLot()
+    {
+        string loot;
+        switch (Random.Range(0, possible.Count+1))
+        {
+            case <=5:
+                loot = possible[0];
+                break;
+            case <=7:
+                loot = possible[6];
+                break;
+            case 8:
+                loot = possible[8];
+                break;
+            case 9:
+                loot = possible[9];
+                break;
+            case 11:
+                loot = possible[10];
+                break;
+            default:
+                loot = possible[0];
+                break;
+        }
+        return loot;
     }
 }
